c370805e3243192af6c5531ebce95282446955ef
Esteban Meza <estebanmeza@devneedzaio.com>
2022-12-14 12:00:33 -0500

    create app and user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    required: true,
+  })
+  userId: number;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  name: number;
+
+  @property({
+    type: 'number',
+  })
+  telephone?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
